73284a4b7ca723a08b5b9feed8e94619c35c75cf
Luiss0704 <luisfgns@gmail.com>
2026-01-20 17:49:20 -0600

    Agregué validaciones a las entradas de datos de cada calificación y mensajes de error

--- 02-EstructurasDeControl/programasJava/Calificaciones.java
+++ 02-EstructurasDeControl/programasJava/Calificaciones.java
@@ -4,18 +4,41 @@ import javax.swing.JOptionPane;
 public class Calificaciones {
     public static void main(String[] args) {
         // Declaración de variables
-        double c1, c2, c3, prom;
+        double c1 = 11.0, c2 = 0.0, c3 = 11.0, prom;
 
         // Solicitar calificaciones
-        c1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la primera calificación:", "Calificación 1", JOptionPane.QUESTION_MESSAGE));
-        c2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la segunda calificación:", "Calificación 2", JOptionPane.QUESTION_MESSAGE));
-        c3 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la tercera calificación:", "Calificación 3", JOptionPane.QUESTION_MESSAGE));
+        // Ciclo para validar la primera entrada de datos
+        while (c1 < 0.0 || c1 > 10.0) {
+            c1 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la primera calificación:", "Calificación 1", JOptionPane.QUESTION_MESSAGE));
+            if(c1 < 0  || c1 > 10) {        // If para mostrar un error y repetir el ciclo
+                JOptionPane.showMessageDialog(null, "¡Dato no válido!", "ERROR", JOptionPane.ERROR_MESSAGE);
+                c1 = 11.0;
+            }
+        }
+
+        // Ciclo para validar la segunda entrada de datos
+        while (c2 < 0.0 || c2 > 10.0) {
+            c2 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la segunda calificación:", "Calificación 2", JOptionPane.QUESTION_MESSAGE));
+            if(c2 < 0  || c2 > 10) {        // If para mostrar un error y repetir el ciclo
+                JOptionPane.showMessageDialog(null, "¡Dato no válido!", "ERROR", JOptionPane.ERROR_MESSAGE);
+                c2 = 11.0;
+            }
+        }
+
+        // Ciclo para validar la tercera entrada de datos
+        while (c3 < 0.0 || c3 > 10.0) {
+            c3 = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la tercera calificación:", "Calificación 3", JOptionPane.QUESTION_MESSAGE));
+            if(c3 < 0  || c3 > 10) {        // If para mostrar un error y repetir el ciclo
+                JOptionPane.showMessageDialog(null, "¡Dato no válido!", "ERROR", JOptionPane.ERROR_MESSAGE);
+                c3 = 11.0;
+            }
+        }
 
         // Calcular promedio
         prom = (c1 + c2 + c3) / 3;
 
         // Mostrar el promedio y determinar si el alumno aprueba o reprueba
-        if (prom > 7) {     // Inicio del if
+        if (prom >= 7) {     // Inicio del if
            JOptionPane.showMessageDialog(null, String.format("Resultado: %.2f", prom) + "\nAlumno APROBADO", "APROBADO", JOptionPane.INFORMATION_MESSAGE);
         } else {       // Inicio del else
             JOptionPane.showMessageDialog(null, String.format("Resultado: %.2f", prom) + "\nAlumno REPROBADO", "REPROBADO", JOptionPane.ERROR_MESSAGE);
